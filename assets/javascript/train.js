ff66a32056b1f61dffbe1eca7c3d7bdd923c5295
Baran Birol <baranbirol@barans-mbp.lan>
2018-04-10 20:52:42 -0700

    firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyB6GXe7gkEjTZtJQx2z2vrTALS7_qFrFco",
+        authDomain: "test-firebase-bd806.firebaseapp.com",
+        databaseURL: "https://test-firebase-bd806.firebaseio.com",
+        projectId: "test-firebase-bd806",
+        storageBucket: "test-firebase-bd806.appspot.com",
+        messagingSenderId: "593832170791"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+
+
+});
